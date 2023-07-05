a2363ce5ba321f5aea745a1b432dce38cf68faa5
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-07-05 16:33:30 -0300

    substuido o assunto pela opção para html e alterado enviarEmail para true

--- enviando-email2/src/test/java/enviandoEmail2/AppTest.java
+++ enviando-email2/src/test/java/enviandoEmail2/AppTest.java
@@ -8,12 +8,24 @@ public class AppTest {
 	@Test
     public void TesteEmail() throws Exception{
 		
+		StringBuilder stringBuilderTextoEmail = new StringBuilder();
+		
+		stringBuilderTextoEmail.append("<h2>Como Vai, Segue abaixo</h2>");
+		stringBuilderTextoEmail.append("<h2>algumas informações minhas</h2><br/>");
+		stringBuilderTextoEmail.append("<h3 style=\"color: rgb(105, 105, 208)\">Celular: 97547-8395<br/>");
+		stringBuilderTextoEmail.append("Email Príncipal: nelsonscosta@yahoo.com.br<br/>");
+		stringBuilderTextoEmail.append("https://www.linkedin.com/in/nelson-souto-itil-083b7b23/<br/>");
+		stringBuilderTextoEmail.append("https://github.com/nelsonscosta/Projeto-Spring-Boot/tree/master</h3><br/>");
+		
+		
+		
 		ObjetoenvioEmail2 enviaEmail = new ObjetoenvioEmail2("nelsonscosta@yahoo.com.br",
 															  "Nelson - Programador em Java",
 															  "Email enviado através do java",
-															  "Email de teste");
-		//no caso ainda não estamos usando html
-		enviaEmail.enviarEmail(false);
+															   stringBuilderTextoEmail.toString());
+		//quando enviar email html será true
+		//quando enviar email somente texto será false
+		enviaEmail.enviarEmail(true);
     	
 		//caso o e-mail não está sendo enviado colocar um tempo
 		Thread.sleep(2000);	
